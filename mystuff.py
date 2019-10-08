93733c1fa6b75aadf5ee4b47a8bf9bcc8f8425a2
raymondmar61 <raym61@hotmail.com>
2019-10-08 16:46:12 -0700

    upload

---
+++ mystuff.py
@@ -0,0 +1,10 @@
+def apple():
+	print("I am apples.  apple() function from mystuff.py python file.")
+
+tangerine = "Living reflection of a dream"
+
+class MyStuff(object):
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+	def apple(self):
+		print("I am classy applies in class MyStuff from mystuff.py python file.")
